2d6b8fac1567f925e0b291946d392b13e9fdca84
reorj <jeff10.rj@gmail.com>
2020-08-12 20:52:56 -0400

    Add project files.

---
+++ WellBeings NetCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WellBeings_NetCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
